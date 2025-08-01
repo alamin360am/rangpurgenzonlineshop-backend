84dc0ff55258d9386e3ece49c9c6eb8870436c3d
alamin360am <alaminhimu0@gmail.com>
2025-08-02 03:06:02 +0600

    bug fixed

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -18,7 +18,7 @@ export default userRouter;
 // import rateLimit from "express-rate-limit"
 
 // export const loginLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 মিনিট
-//   max: 5, // ৫ বার ট্রাই করা যাবে
+//   windowMs: 15 * 60 * 1000,
+//   max: 5,
 //   message: "Too many login attempts. Please try again later."
 // });
